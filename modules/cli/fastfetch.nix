e575421d5f6186219b4a06f6ab4a13a816e7793c
Will McCallion <will.mccallion@icloud.com>
2026-03-14 20:14:56 -0600

    Cleaned up fast fetch.

--- modules/cli/fastfetch.nix
+++ modules/cli/fastfetch.nix
@@ -12,7 +12,7 @@
 	xdg.configFile."fastfetch/config.jsonc".text = builtins.toJSON {
 		"$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
 		logo = {
-			type = "small";
+			source = "nixos";
 			color = {
 				"1" = "blue";
 				"2" = "cyan";
@@ -20,6 +20,9 @@
 		};
 		display = {
 			separator = "  ";
+			key = {
+				width = 8;
+			};
 			color = {
 				keys = "blue";
 				title = "cyan";
@@ -29,7 +32,6 @@
 			{
 				type = "title";
 				format = "{user-name}@{host-name}";
-				key = " ";
 			}
 			{
 				type = "separator";
@@ -37,48 +39,48 @@
 			}
 			{
 				type = "os";
-				key = "  os";
+				key = "os";
 			}
 			{
 				type = "kernel";
-				key = "  kernel";
+				key = "kernel";
 			}
 			{
 				type = "packages";
-				key = " 󰏖 pkgs";
+				key = "pkgs";
 			}
 			{
 				type = "shell";
-				key = "  shell";
+				key = "shell";
 			}
 			{
 				type = "wm";
-				key = " 󰖲 wm";
+				key = "wm";
 			}
 			{
 				type = "terminal";
-				key = "  term";
+				key = "term";
 			}
 			{
 				type = "cpu";
-				key = " 󰻠 cpu";
+				key = "cpu";
 			}
 			{
 				type = "gpu";
-				key = " 󰍹 gpu";
+				key = "gpu";
 			}
 			{
 				type = "memory";
-				key = "  mem";
+				key = "memory";
 			}
 			{
 				type = "disk";
-				key = " 󰋊 disk";
+				key = "disk";
 				folders = "/";
 			}
 			{
 				type = "uptime";
-				key = " 󰅐 up";
+				key = "uptime";
 			}
 			"break"
 			{
