2cf3bd87a77ad1dc27ea431c5f6712efa961022e
Will McCallion <will.mccallion@icloud.com>
2026-04-30 14:37:38 -0600

    Fixed proton pass extension.

--- modules/apps/browser.nix
+++ modules/apps/browser.nix
@@ -50,7 +50,7 @@
           installation_mode = "force_installed";
         };
         # Proton Pass
-        "78272b6fa58f4a1abaac99321d503a20@proton.ch" = {
+        "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
           installation_mode = "force_installed";
         };
