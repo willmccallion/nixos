89508010fd7be78359bdec7dd4a0a9108595b4e8
Will McCallion <will.mccallion@icloud.com>
2026-04-29 22:13:14 -0600

    Made default firefox setup better.

--- modules/apps/browser.nix
+++ modules/apps/browser.nix
@@ -1,5 +1,86 @@
 { pkgs, ... }:
 
 {
-	programs.firefox.enable = true;
+  programs.firefox = {
+    enable = true;
+
+    # Enterprise policies — applied to all profiles, can't be overridden
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableFormHistory = true;
+      DisablePasswordReveal = true;
+      DontCheckDefaultBrowser = true;
+      OfferToSaveLogins = false;
+      PasswordManagerEnabled = false;
+
+      # HTTPS-only and DNS over HTTPS
+      DNSOverHTTPS = {
+        Enabled = true;
+        ProviderURL = "https://dns.quad9.net/dns-query";
+        Locked = false;
+      };
+      HttpsOnlyMode = "force_enabled";
+
+      # Block tracking
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+
+      # Auto-install extensions
+      ExtensionSettings = {
+        # uBlock Origin
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        # Privacy Badger
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        # Dark Reader
+        "addon@darkreader.org" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        # Proton Pass
+        "78272b6fa58f4a1abaac99321d503a20@proton.ch" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+
+      # Don't send anything extra to Mozilla
+      FirefoxHome = {
+        Search = true;
+        TopSites = false;
+        SponsoredTopSites = false;
+        Highlights = false;
+        Pocket = false;
+        SponsoredPocket = false;
+      };
+
+      SearchSuggestEnabled = false;
+    };
+
+    # Lower-level about:config prefs
+    preferences = {
+      "privacy.donottrackheader.enabled" = true;
+      "privacy.trackingprotection.enabled" = true;
+      "privacy.trackingprotection.socialtracking.enabled" = true;
+      "privacy.firstparty.isolate" = true;
+      "network.cookie.cookieBehavior" = 5; # Reject cross-site trackers
+      "browser.send_pings" = false;
+      "beacon.enabled" = false;
+      "dom.battery.enabled" = false;
+      "media.navigator.enabled" = false;
+      "geo.enabled" = false;
+    };
+  };
 }
